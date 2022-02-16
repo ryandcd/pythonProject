f016a2de95cd1e9a8d507de2c5bb26a5e2b325e8
ryandcd <pistonsfan1232@gmail.com>
2022-02-15 22:31:26 -0500

    bb function created - proof of concept for embedded python functions!!

--- main.py
+++ main.py
@@ -1,16 +1,33 @@
-# This is a sample Python script.
+# Ryan Croos
+# 2/15/2021
+# Lets mess around
+# TO DO
+# implement win counter
+# fix function return if user puts in an invalid input to user
+# make this more robust
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# import xlrd
+# import xlwt
+import random # for random number generator
+import time # for implementing time delays for printing in terminal
+
+def bby_function(name, bgo, bleh):
+    print(f'Hello, {name}\n')
+    time.sleep(1) # create computer thinking effect
+    print(f'It seems that I am talking to {bgo}!!!!!!\n')
+    time.sleep(1) # create computer thinking effect
+    if len(bleh) == 3:
+        print(f'Why are you {bleh}!!!\n')
+    elif bleh == 'not bleh': # make ROBUST
+        print(f'I am happy that you are not bleh, {bgo}')
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    x = input('What is your real name? ')
+    y = input('Are you bingo or bungo? ')
+    z = input('Bleh or not bleh? ')
+    bby_function(x, y, z)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+
